53dbebbc4e76762e22bbc0130f1f7a370fdfd0d2
lx-love <97000433@qq.com>
2017-08-16 14:15:37 +0800

    ss

--- application/views/particulars.php
+++ application/views/particulars.php
@@ -9,11 +9,21 @@
     <script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js" ></script>
     <script src="http://apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-    <link href="<?php echo base_url() ?>/css/particulars.css" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="<?php echo base_url() ?>/js/particulars.js"></script>
+    <link href="<?php echo base_url()?>/css/particulars.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="<?php echo base_url()?>/js/particulars.js"></script>
     <title>详情</title>
 </head>
 <body>
+<div id="dingbu">
+<div class="container" >
+    <div class='row hidden-xs'>
+        <div class='col-lg-3   col-xs-12'>我是内容</div>
+        <div class='col-lg-3   col-xs-12'>我是内容</div>
+        <div class='col-lg-3   col-xs-12'>我是内容</div>
+        <div class='col-lg-3   col-xs-12'>我是内容</div>
+  </div>
+</div>
+</div>
 
 </body>
 </html>
