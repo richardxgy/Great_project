da1791a3fd8aa3ecca6d9447e3e093e9ef572cec
Your Name <you@example.com>
2017-08-17 11:19:33 +0800

    购物车4次

--- application/views/shoppingCar.php
+++ application/views/shoppingCar.php
@@ -4,33 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://unpkg.com/vue"></script>
     <link href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js" ></script>
+    <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="http://apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
     <link href="<?php echo base_url() ?>/css/shoppingCar.css" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="<?php echo base_url() ?>/js/shoppingCar.js"></script>
+
+
+
     <title>购物车</title>
 </head>
 <body>
-
+<div id="app">
 <nav class="navbar navbar-default  nav1 hidden-xs">
     <div class="container">
-        <ul class="row" >
-            <li  class="col-md-2 "><span class="glyphicon glyphicon-home" aria-hidden="true"></span>首页</li>
-            <li  class="col-md-2 "><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>地区</li>
-            <li  class="col-md-2 "><span class="glyphicon glyphicon-file" aria-hidden="true"></span>账号</li>
-            <li  class="col-md-2 "><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>我的订单</li>
-            <li  class="col-md-2 "><span class="glyphicon glyphicon-user" aria-hidden="true"></span>个人中心</li>
-            <li  class="col-md-2 "><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>购物车</li>
+        <ul class="row">
+            <li class="col-md-2 "><span class="glyphicon glyphicon-home" aria-hidden="true"></span>首页</li>
+            <li class="col-md-2 "><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>地区</li>
+            <li class="col-md-2 "><span class="glyphicon glyphicon-file" aria-hidden="true"></span>账号</li>
+            <li class="col-md-2 "><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>我的订单</li>
+            <li class="col-md-2 "><span class="glyphicon glyphicon-user" aria-hidden="true"></span>个人中心</li>
+            <li class="col-md-2 "><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>购物车</li>
         </ul>
     </div>
 </nav>
 
 <!--搜索框-->
 <div class="container searchdiv">
-    <img src="<?php echo base_url()?>/images/logo.png" alt="" class="col-md-2 col-xs-4">
+    <img src="<?php echo base_url() ?>/images/logo.png" alt="" class="col-md-2 col-xs-4">
     <input type="text" placeholder="搜索" class="col-md-8 col-xs-5 searchinput">
     <button class="col-md-1 col-xs-3 searchbutton">搜索</button>
 </div>
@@ -45,40 +48,44 @@
         <th class="col-md-1 hidden-xs">操作</th>
         </thead>
         <tbody>
-        <thead class="row">
-            <td class="col-md-4 col-xs-3"><div class="row">
-                    <input type="checkbox" class="col-md-1 col-xs-2">
-                    <img src="<?php echo base_url()?>/images/kouhong.jpg" alt="" class="col-md-3 col-xs-12">
-                    <p class="col-md-4 col-xs-12">良品铺子 手剥松子</p>
-                    <p class="col-md-4 col-xs-12">包装：裸装</p>
+        <thead class="row" v-for="(x,index) in list">
+        <td class="col-md-4 col-xs-3">
+            <div class="row">
+                <input type="checkbox" class="col-md-1 col-xs-2" v-model="x.bol" v-on:click="check(x)">
+                <img src="<?php echo base_url() ?>/images/kouhong.jpg" alt="" class="col-md-3 col-xs-12">
+                <p class="col-md-4 col-xs-12">良品铺子 手剥松子</p>
+                <p class="col-md-4 col-xs-12">{{x.information}}</p>
 
-                </div></td>
-            <td class="col-md-1 col-xs-1">￥13</td>
-            <td class="col-md-1 col-xs-1 row">
-                <button class="col-md-1 col-xs-1 hidden-xs" >-</button>
-                <input type="text" value="2" class="col-md-4 col-xs-12" style="padding: 1px">
-                <button class="col-md-1 col-xs-1 hidden-xs">+</button>
-                </td>
-            <td class="col-md-1 col-xs-1">￥26</td>
-            <td class="col-md-1 col-xs-1 hidden-xs"><p>移入收藏夹</p><p>删除</p></td>
+            </div>
+        </td>
+        <td class="col-md-1 col-xs-1">￥{{x.price}}</td>
+        <td class="col-md-1 col-xs-1 row">
+            <button class="col-md-1 col-xs-1 hidden-xs" v-on:click="reduction(x,index)">-</button>
+            <input type="text" v-model="x.number"class="col-md-4 col-xs-12" style="padding: 1px">
+            <button class="col-md-1 col-xs-1 hidden-xs" v-on:click="add(x,index)">+</button>
+        </td>
+        <td class="col-md-1 col-xs-1">￥{{x.amount}}</td>
+        <td class="col-md-1 col-xs-1 hidden-xs"><p>移入收藏夹</p>
+            <p v-on:click="del(index)">删除</p></td>
         </thead>
-
         </tbody>
-
     </table>
-
 
 </div>
 <div class="chekall container">
 
-    <p class="col-md-1 col-xs-3"><input type="checkbox" >全选</p>
-    <p class="col-md-1 col-xs-3" >删除</p>
+    <p class="col-md-1 col-xs-3"><input type="checkbox" v-model="cb" v-on:click="chall">全选</p>
+    <p class="col-md-1 col-xs-3">删除</p>
     <p class="col-md-5  hidden-xs">移入收藏夹</p>
     <p class="col-md-2 col-xs-6">已选商品：2件</p>
-    <p class="col-md-2 col-xs-5">合计：￥300</p>
-    <button class="col-md-1 col-xs-3">结算</button>
+    <p class="col-md-2 col-xs-5">合计：￥{{total}}</p>
+    <button class="col-md-1 col-xs-3" id="btn1" onclick="fn()">结算</button>
 
 </div>
+
 <hr>
+</div>
+<script type="text/javascript" src="<?php echo base_url() ?>js/shoppingCar.js"></script>
+
 </body>
 </html>
